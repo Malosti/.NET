336dca4f1a1101e6452629c29942b970299923e3
Guilherme Malosti Fonseca <gmathos77@gmail.com>
2023-06-01 16:43:38 -0300

    Maipulando arquivos e diretorios

---
+++ Diretorios/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+var salesFile = FindFile("stores");
+
+foreach(var file in salesFile){
+    Console.WriteLine(file);
+}
+
+IEnumerable<string> FindFile(string folderName){
+    List<string> salesFile = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach(var file in foundFiles){
+        if(file.EndsWith("sales.json")){
+            salesFile.Add(file);
+        }
+    }
+    return salesFile;
+}
