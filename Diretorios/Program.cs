e4ae6b4da4e3b9e2a162bc41a796647694a99f06
Guilherme Malosti Fonseca <gmathos77@gmail.com>
2023-06-05 15:56:33 -0300

    adicionando texto e trabalhando com json

--- Diretorios/Program.cs
+++ Diretorios/Program.cs
@@ -1,23 +1,22 @@
 ﻿using System.IO;
 using System.Collections.Generic;
-/*
+using Newtonsoft.Json;
+
+var salesJson = File.ReadAllText($"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales.json");
+var salesData = JsonConvert.DeserializeObject<SalesTotal>(salesJson);
+Console.WriteLine(salesData?.Total);
+
+
 var currentDirectory = Directory.GetCurrentDirectory();
-var storesDirectory = Path.Combine(currentDirectory, "stores", "201", "newDir");
+var storesDirectory = Path.Combine(currentDirectory, "stores");
 var salesFile = FindFile(storesDirectory);
 
-foreach(var file in salesFile){
-    Console.WriteLine(file);
-}
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);
 
-//Verifica se existe a o diretorio passado
-bool doesDirectioryExist = Directory.Exists(storesDirectory);
-System.Console.WriteLine(doesDirectioryExist);
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesData?.Total}{Environment.NewLine}");
 
-// Cria uma nova pasta
-//Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "stores", "201", "newDir"));
 
-//Cria um novo arquivo
-File.WriteAllText(Path.Combine(storesDirectory, "greeting.txt"), "Hello World!");
 
 // Método localiza os arquivos .json na pasta stores
 IEnumerable<string> FindFile(string folderName){
@@ -33,29 +32,21 @@ IEnumerable<string> FindFile(string folderName){
     }
     return salesFile;
 }
+
+
+/*
+//Verifica se existe a o diretorio passado
+bool doesDirectioryExist = Directory.Exists(storesDirectory);
+System.Console.WriteLine(doesDirectioryExist);
+
+// Cria uma nova pasta
+//Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "stores", "201", "newDir"));
+
+//Cria um novo arquivo
+File.WriteAllText(Path.Combine(storesDirectory, "greeting.txt"), "Hello World!");
 */
 
-var currentDirectory = Directory.GetCurrentDirectory();
-var storesDirectory = Path.Combine(currentDirectory, "stores");
 
-var salesFile = FindFile(storesDirectory);
-
-foreach(var file in salesFile){
-    System.Console.WriteLine(file);
+class SalesTotal{
+    public double Total { get; set; }
 }
-
-IEnumerable<string> FindFile(string folderName){
-    List<string> salesFile = new List<string>();
-
-    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
-
-    foreach(var file in foundFiles){
-        var extension = Path.GetExtension(".json");
-        if(extension == ".json"){
-            salesFile.Add(file);
-        }
-    }
-
-    return salesFile;
-}
-
